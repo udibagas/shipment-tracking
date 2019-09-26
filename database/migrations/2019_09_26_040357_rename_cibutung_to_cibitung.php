e4a19902114f8334cf25d2a0e9b6bb77fcba85d3
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-09-26 11:04:52 +0700

    update cibutung to cibitung

---
+++ database/migrations/2019_09_26_040357_rename_cibutung_to_cibitung.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\City;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameCibutungToCibitung extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            City::where('name', 'CIBUTUNG')->update(['name' => 'CIBITUNG']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            //
+        });
+    }
+}
