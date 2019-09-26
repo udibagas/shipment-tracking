ee2d65af6d5cdccb7e35dcf9441128c78f8cebab
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-09-26 10:52:43 +0700

    tambah kota

---
+++ database/migrations/2019_09_26_034153_add_cities.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\City;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCities extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            City::insert([
+                ['name' => 'ADARO'],
+                ['name' => 'SAMPIT'],
+                ['name' => 'SANGATTA'],
+                ['name' => 'TANJUNG ENIM'],
+                ['name' => 'TANJUNG PANDAN'],
+                ['name' => 'CIBUTUNG'],
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            //
+        });
+    }
+}
