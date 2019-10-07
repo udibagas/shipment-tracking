394a2bdd3f4cc31bd44f7e9c490596799a26357f
Bagas Udi Sahsangka <udibagas@gmail.com>
2019-10-07 14:48:07 +0700

    benahi blade email

--- resources/views/emails/domestic_delivery_report.blade.php
+++ resources/views/emails/domestic_delivery_report.blade.php
@@ -7,7 +7,7 @@ Berikut kami sampaikan update laporan pengiriman barang periode tanggal {{ date(
 | Nomor SPB/Resi | Tujuan | Layanan | Jml Koli | Berat | Tgl Pick Up | ETD | Tgl Kirim | ETA | Tgl Terima | Status | Waktu Update |
 | --- | --- | --- | :---: | :---: | :---: | :---: | :---: | :---: | :---: | :---: | :---: |
 @foreach ($data->items as $i)
-| {{ $i['spb_number'] }} <br> {{ $i['resi_number'] }} | {{ $i['destination']}} <br> {{ $i['delivery_address'] }} | {{ $i['service_type'] }} <br> {{ $i['vehicle_type'] }} | {{ $i['quantity'] }} | {{ $i['invoice_weight'] }} KG | {{ $i['pick_up_date'] }} | {{ $i['etd'] }} | {{ $i['delivery_date'] }} | {{ $i['eta'] }} | {{ $i['delivered_date'] }} | {{ $i['statusName'] }} | {{ $i['updated_at'] }} |
+| {{ $i['spb_number'] }} <br> {{ $i['resi_number'] }} | {{ $i['destination']}} <br> {{ $i['delivery_address'] }} | {{ $i['service_type'] }} <br> {{ $i['vehicle_type']['name'] }} | {{ $i['quantity'] }} | {{ $i['invoice_weight'] }} KG | {{ $i['pick_up_date'] }} | {{ $i['etd'] }} | {{ $i['delivery_date'] }} | {{ $i['eta'] }} | {{ $i['delivered_date'] }} | {{ $i['statusName'] }} | {{ $i['updated_at'] }} |
 @endforeach
 @endcomponent
 
