179dcbe524b3d0861ee83f708c4a9f859a44b1cc
Bagas Udi Sahsangka <udibagas@gmail.com>
2020-02-01 00:58:14 +0700

    api ok

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::post('login', 'AuthController@login');
-Route::group(['middleware' => 'auth.jwt'], function() {
+Route::group(['middleware' => 'auth'], function() {
     Route::post('logout', 'AuthController@logout');
     Route::get('domesticDelivery/search', 'DomesticDeliveryController@searchApi');
     Route::post('deliveryProgress', 'DeliveryProgressController@store');
