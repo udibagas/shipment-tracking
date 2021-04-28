ee6e5a9640272f6a98d2e16bfe863d11da746e21
Bagas Udi Sahsangka <bagas@lamsolusi.com>
2021-04-28 21:21:12 +0700

    new auth method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,84 +4,36 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
-use JWTAuth;
-use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth', ['except' => ['login']]);
-    }
-
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function login(Request $request)
     {
-        $credentials = $request->only(['email', 'password']);
-        $credentials['active'] = 1;
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
 
-        if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['message' => 'Invalid Email or Password'], 401);
+        $auth = Auth::attempt($request->only(['email', 'password']));
+
+        if ($auth) {
+            $user = User::find(auth()->id());
+            $user->token = $user->createToken($request->device_name ?: 'web')->plainTextToken;
+            return $user;
         }
 
-        return $this->respondWithToken($token);
+        return response(['message' => 'Wrong username or password'], 401);
     }
 
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token)
+    public function logout(Request $request)
     {
-        return response()->json([
-            'user' => auth()->user(),
-            'token' => $token,
-            // 'token_type' => 'bearer',
-            // 'expires_in' => auth()->factory()->getTTL() * 60
-        ])->cookie('token', $token);
+        $request->user()->currentAccessToken()->delete();
+        return response('', 204);
     }
 
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function logout()
-    {
-        auth()->logout();
-
-        return response()->json(['message' => 'Successfully logged out']);
-    }
-
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function me()
     {
-        return response()->json(auth()->user());
-    }
-
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function refresh()
-    {
-        return $this->respondWithToken(auth()->refresh());
+        return auth()->user();
     }
 }
